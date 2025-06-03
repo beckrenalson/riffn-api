e2952eb1a1dc95c03624b97962f3002cba001d12
beckrenalson <beck.take2@gmail.com>
2025-06-03 16:27:02 +1200

    port

--- index.js
+++ index.js
@@ -124,6 +124,8 @@ app.post('/signup', async (req, res) => {
 //     res.json(student);
 // });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
   console.log('Server running on port 3000');
 });
